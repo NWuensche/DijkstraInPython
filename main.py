1322a94bb72a1c1131cf5276d5fba3495fcad875
Felix Cornelius <mail@felixcornelius.de>
2016-02-24 21:51:55 +0100

    adjacency matrix fix

--- main.py
+++ main.py
@@ -3,9 +3,18 @@
 
 from parser import *
 
+def print_matrix(m):
+    for i in m:
+        for j in i:
+            print('{0:3d}'.format(j),end='')
+        print ('', end='\n')
+
+
 def main():
     p = InputParser()
-    print(p.get_input())
+    adj = p.get_input()
+    print("\nAdjacency matrix is:\n")
+    print_matrix(adj)
 
 if __name__ == '__main__':
     main()
--- parser.py
+++ parser.py
@@ -1,23 +1,38 @@
+# -*- coding: utf-8 -*-
+
 class InputParser:
     def __init__(self):
         pass
 
     def get_input(self):
+
         print("Please enter vertices and edge weights")
         print("(format: [[start,weight,end],[...],...]")
         inp = input()
 
+        # Input als Liste von Kanten ([a,x,b]) parsen
+        # TODO: Fehleingaben berücksichtigen
         edges = ''.join(inp.split(']')).split('[')
         edges = [x.split(',') for x in edges if x]
         edges[0] = edges[0][0:3]
 
-        verts = sorted(list({x[0] for x in edges}))
+        # Ziel- und Endknoten als set zusammenfassen,
+        # dann in Liste sortieren
+        verts_out = {x[0] for x in edges}
+        verts_in  = {x[2] for x in edges}
+        verts_out.update(verts_in)
+        verts = sorted(list(verts_out))
+
         return self.adjmatrix(verts,edges)
 
     def adjmatrix(self, verts, edges):
+
+        # nxn Nullmatrix initialisieren mit n=|verts|
         size = len(verts)
         matrix = [[ 0 for i in range(size)] for j in range(size)]
 
+        # Ausgehende Kanten für jeden Knoten bestimmen,
+        # zugehörige gewichte in adj-Matrix speichern
         for v in verts:
             adj = [e for e in edges if e[0]==v]
             for a in adj:
