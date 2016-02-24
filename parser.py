8b1611b5621b1881a523f9158022eeeb3593d308
Felix Cornelius <mail@felixcornelius.de>
2016-02-24 20:35:13 +0100

    parser returns adjacency matrix

--- parser.py
+++ parser.py
@@ -6,11 +6,22 @@ class InputParser:
         print("Please enter vertices and edge weights")
         print("(format: [[start,weight,end],[...],...]")
         inp = input()
+
         edges = ''.join(inp.split(']')).split('[')
         edges = [x.split(',') for x in edges if x]
         edges[0] = edges[0][0:3]
 
         verts = sorted(list({x[0] for x in edges}))
-        print (verts)
-        return edges
+        return self.adjmatrix(verts,edges)
 
+    def adjmatrix(self, verts, edges):
+        size = len(verts)
+        matrix = [[ 0 for i in range(size)] for j in range(size)]
+
+        for v in verts:
+            adj = [e for e in edges if e[0]==v]
+            for a in adj:
+                matrix[verts.index(v)][verts.index(a[2])] = \
+                    int(a[1])
+
+        return matrix
