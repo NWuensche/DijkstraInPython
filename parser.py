9992dc9a98bc7414b18c01ff8ac63299d5a5d487
Niklas Wuensche <Niklas.Wuensche@mailbox.tu-dresden.de>
2016-02-25 12:25:35 +0100

    check, if syntax of input is right

--- parser.py
+++ parser.py
@@ -11,26 +11,28 @@ class InputParser:
         inp = input()
 
         # Input als Liste von Kanten ([a,x,b]) parsen
-        # TODO: Fehleingaben berücksichtigen
         edges = ''.join(inp.split(']')).split('[')
         edges = [x.split(',') for x in edges if x]
-        edges[0] = edges[0][0:3]
 
-        # Ziel- und Endknoten als set zusammenfassen,
-        # dann in Liste sortieren
-        verts_out = {x[0] for x in edges}
-        verts_in  = {x[2] for x in edges}
-        verts_out.update(verts_in)
-        verts = sorted(list(verts_out))
+        #Schaut, ob Input fehlerfrei ist
+        if self.check_edges(edges):
+            edges[0] = edges[0][0:3]
 
-        return self.adjmatrix(verts,edges)
+            # Ziel- und Endknoten als set zusammenfassen,
+            # dann in Liste sortieren
+            verts_out = {x[0] for x in edges}
+            verts_in  = {x[2] for x in edges}
+            verts_out.update(verts_in)
+            verts = sorted(list(verts_out))
+            return self.adjmatrix(verts,edges)
+        else:
+            return self.get_input()
 
     def adjmatrix(self, verts, edges):
 
         # nxn Nullmatrix initialisieren mit n=|verts|
         size = len(verts)
         matrix = [[ 0 for i in range(size)] for j in range(size)]
-
         # Ausgehende Kanten für jeden Knoten bestimmen,
         # zugehörige gewichte in adj-Matrix speichern
         for v in verts:
@@ -40,3 +42,24 @@ class InputParser:
                     int(a[1])
 
         return matrix
+
+    def check_edges(self,edges):
+         #Schaut, ob zu viele oder zu wenig Argumente und ob alles Zahlen sind
+         for edge in edges:
+            # Edge, die 3 Zahlen hatte,
+            #      kann ich verarbeitung auch "" als 4te Stelle habem, aber dennoch
+            #      richtig sein => berücksichtigen
+            if len(edge) == 4 and edge[3] == "":
+                continue
+            elif len(edge)>3 and edge[3]!="":
+                print("Invalid Syntax: Too many arguments!")
+                return False
+            elif len(edge)<3:
+                print("Invalid Syntax: Too few arguments!")
+                return False
+            else:
+                for item in edge:
+                    if(not item.isdigit()):
+                        print("Invalid Syntax: Just digits allowed!")
+                        return False
+            return True
