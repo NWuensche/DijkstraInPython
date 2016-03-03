d1f32e4fb4b604106fc7844cee532ea6cee90b85
Niklas Wuensche <Niklas.Wuensche@mailbox.tu-dresden.de>
2016-03-03 10:34:56 +0100

    add new dijkstra to main

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 from parser import *
 from calculate_shortest_way import *
 from dijkstra import *
+from dijkstraNew import *
 
 # Matrix mit Unicode multiline braces printen
 def print_matrix(m):
@@ -20,7 +21,6 @@ def main():
     adj = p.get_matrix()
     nodes = p.nodes
     v1, v2 = p.get_path()
-
     print("\nAdjacency matrix is:\n")
     print_matrix(adj)
     print("The graph has {details}".format(details=p.graph_info(adj)))
@@ -28,12 +28,13 @@ def main():
     d = Dijkstra(adj,nodes,v1)
     print("Shortest way {0} \u279c {1} (Dijkstra): ".format(v1,v2),end='')
     print(d.dist_to(v2))
-    
-    # kürzesten Weg zwischen v1 und v2 ausgeben
-    shortest_way = d.path_to(v2)
-    for node in range(len(shortest_way)-1):
-        print("{0} \u279c ".format(shortest_way[node]),end="")
-    print(shortest_way[-1])
+
+    # Neuer Dijkstra
+    d2 = DijkstraNew(p.edges,v1)
+    print("Kürzester Weg")
+    print(d2.get_shortest_way(v2))
+    print("andere kürzeeste Wege")
+    #d2.print_list()
 
     print("\nOther distances from {0}:".format(v1))
     d.print_list()
